2c42e1a8908e74f39b11360893d77519cd4c36d9
Javier <jfernandezfernandez.sanjose@alumnado.fundacionloyola.net>
2019-12-12 12:26:59 +0100

    Add methods: setAllDataConnection setProperties receiveAllEmails

--- royalHospital/src/main/java/com/royalhospital/royalHospital/MailMethods.java
+++ royalHospital/src/main/java/com/royalhospital/royalHospital/MailMethods.java
@@ -4,7 +4,7 @@ import java.util.Properties;
 
 import javax.mail.Folder;
 
-import com.mysql.cj.xdevapi.Session;
+import javax.mail.Session;;
 
 public class MailMethods {
 	private String host;
@@ -34,7 +34,14 @@ public class MailMethods {
 	}
 	
 	private void setProperties() {
-		
+		try {
+			properties.put("mail.pop3.host", host);
+			properties.put("mail.pop3.port", "995");
+			properties.put("mail.pop3.starttls.enable", "true");	
+			session = Session.getDefaultInstance(properties);
+		} catch (Exception e) {
+			System.out.println("Error setting properties about E-mail");
+		}
 	}
 
 	public String getHost() {

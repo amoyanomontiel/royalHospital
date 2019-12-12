8a52674a98b7c3411546af7d7f6a279212399c59
Javier <jfernandezfernandez.sanjose@alumnado.fundacionloyola.net>
2019-12-12 13:17:36 +0100

    Add Methods: storeAllMessages, getBodyText,
    Update Methods:
    storeAllMessages,
    getBodyText,
    reciveAllEmails,
    setFolderEmails

--- royalHospital/src/main/java/com/royalhospital/royalHospital/App.java
+++ royalHospital/src/main/java/com/royalhospital/royalHospital/App.java
@@ -9,5 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        // Test about mail
+//        MailMethods objetoMail = new MailMethods();
+//        objetoMail.setAllDataConnection("pop.gmail.com", "pop3", "email-HERE", "PASSWD");
+//        objetoMail.setProperties();
+//        objetoMail.connectMailServer();
+//        objetoMail.setFolderEmails();
+//        objetoMail.receiveAllEmails();
+//        objetoMail.storeAllMessages();
     }
 }

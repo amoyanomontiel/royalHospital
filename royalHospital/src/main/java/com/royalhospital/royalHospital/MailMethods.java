db99c170084f54dec4441d67100754773096ab02
Javier <jfernandezfernandez.sanjose@alumnado.fundacionloyola.net>
2019-12-12 12:13:19 +0100

    change position of class MailMethods

---
+++ royalHospital/src/main/java/com/royalhospital/royalHospital/MailMethods.java
@@ -0,0 +1,87 @@
+package com.royalhospital.royalHospital;
+
+import java.util.Properties;
+
+import javax.mail.Folder;
+
+import com.mysql.cj.xdevapi.Session;
+
+public class MailMethods {
+	private String host;
+	private String mailStoreType;
+	private String userName;
+	private String passwd;
+	
+	// All properties about connection with E-mail server
+	private Properties properties = new Properties();
+	
+	// Session with E-mail server
+	private Session session;
+	
+	// List of all folders in E-mail
+	private Folder [] folderList;
+	
+	
+	private void receiveAllEmails() {
+		
+	}
+	
+	private void setAllDataConnection(String hostParam, String mailStoreTypeParam, String userNameParam, String passwdParam) {
+	host = hostParam;
+	mailStoreType = mailStoreTypeParam;
+	userName = userNameParam;
+	passwd = passwdParam;
+	}
+	
+	private void setProperties() {
+		
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getMailStoreType() {
+		return mailStoreType;
+	}
+
+	public void setMailStoreType(String mailStoreType) {
+		this.mailStoreType = mailStoreType;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
+	public Session getSession() {
+		return session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
+}

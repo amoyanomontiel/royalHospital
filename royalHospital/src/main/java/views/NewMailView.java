721757858c24c55630b417f637718158787692ea
Fernando Cañadas <fcanadasortega.sanjose@alumnado.fundacionloyola.net>
2019-12-14 22:40:59 +0100

    improvement of the window to send new mails

--- royalHospital/src/main/java/views/NewMailView.java
+++ royalHospital/src/main/java/views/NewMailView.java
@@ -8,22 +8,31 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.Label;
 
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.JScrollBar;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.HashMap;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JComboBox;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
 
 public class NewMailView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField addresseeText;
-	private JTextField subjectText;
 	private final int width = 30;
 	private final int height = 30;
+	private HashMap<String, String> fileList;
 
 	/**
 	 * Launch the application.
@@ -41,14 +50,45 @@ public class NewMailView extends JFrame {
 		});
 	}
 
+	public void createFileChooser(JPanel contentPane) {
+		JFileChooser fc=new JFileChooser();
+		fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fc.setMultiSelectionEnabled(true);
+		int seleccion=fc.showOpenDialog(contentPane);
+		 
+		if (seleccion==JFileChooser.APPROVE_OPTION){
+			File[] selectedFiles = fc.getSelectedFiles();
+			
+			for (int i = 0; i < selectedFiles.length; i++) {
+				if (!fileList.containsKey(selectedFiles[i].getName()));
+				fileList.put(selectedFiles[i].getName(), selectedFiles[i].getAbsolutePath());
+			}
+		}
+	}
+	
+	public void refreshUploadedFiles(JPanel uploadedFilesPanel, GridBagConstraints gblc_uploadedFilesPanel) {
+		int contador = 0;
+		for (String key : fileList.keySet()) {
+			JLabel newFile = new JLabel(key);
+			newFile.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			gblc_uploadedFilesPanel.gridx = 0;
+			gblc_uploadedFilesPanel.gridy = contador;
+			uploadedFilesPanel.add(newFile, gblc_uploadedFilesPanel);
+			contador++;
+		}
+		uploadedFilesPanel.revalidate();
+	}
+	
 	/**
 	 * Create the frame.
 	 */
 	public NewMailView() {
+		fileList = new HashMap<String, String>();
+		
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 850, 617);
-		contentPane = new JPanel();
+		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -58,7 +98,7 @@ public class NewMailView extends JFrame {
 		addresseeLabel.setBounds(67, 29, 102, 27);
 		contentPane.add(addresseeLabel);
 		
-		addresseeText = new JTextField();
+		JTextField addresseeText = new JTextField();
 		addresseeText.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		addresseeText.setBounds(230, 29, 515, 27);
 		contentPane.add(addresseeText);
@@ -69,7 +109,7 @@ public class NewMailView extends JFrame {
 		subjectLabel.setBounds(67, 87, 62, 27);
 		contentPane.add(subjectLabel);
 		
-		subjectText = new JTextField();
+		JTextField subjectText = new JTextField();
 		subjectText.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		subjectText.setColumns(10);
 		subjectText.setBounds(230, 87, 515, 27);
@@ -79,24 +119,43 @@ public class NewMailView extends JFrame {
 		bodyText.setBounds(67, 160, 678, 200);
 		contentPane.add(bodyText);
 		
-		JScrollBar uploadedScroll = new JScrollBar();
-		uploadedScroll.setBounds(278, 405, 467, 48);
-		contentPane.add(uploadedScroll);
-		
 		JLabel uploadedLabel = new JLabel("Archivos Adjuntos");
 		uploadedLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
 		uploadedLabel.setBounds(67, 416, 142, 27);
 		contentPane.add(uploadedLabel);
 		
+		JPanel uploadedFilesPanel = new JPanel();
+		uploadedFilesPanel.setBounds(230, 406, 515, 61);
+		contentPane.add(uploadedFilesPanel);
+		
+		GridBagLayout gbl_uploadedFilesPanel = new GridBagLayout();
+		uploadedFilesPanel.setLayout(gbl_uploadedFilesPanel);
+		GridBagConstraints gblc_uploadedFilesPanel = new GridBagConstraints();
+		gblc_uploadedFilesPanel.weightx = 1;
+		gblc_uploadedFilesPanel.weighty = 1;
+		gblc_uploadedFilesPanel.fill = GridBagConstraints.BOTH;
+		
 		JButton newFileButton = new JButton(" Adjuntar");
+		newFileButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				createFileChooser(contentPane);
+				refreshUploadedFiles(uploadedFilesPanel, gblc_uploadedFilesPanel);
+			}
+		});
 		newFileButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		newFileButton.setBounds(67, 503, 150, 40);
 		ImageIcon fileIcon = new ImageIcon("src//main//java//views//download.png");
 		Icon newFileIcon = new ImageIcon(fileIcon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
+		
 		newFileButton.setIcon(newFileIcon);
 		contentPane.add(newFileButton);
 		
 		JButton sendButton = new JButton(" Enviar");
+		sendButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		sendButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		sendButton.setBounds(595, 503, 150, 40);
 		ImageIcon sendIcon = new ImageIcon("src//main//java//views//send.png");
@@ -105,6 +164,11 @@ public class NewMailView extends JFrame {
 		contentPane.add(sendButton);
 		
 		JButton CancelButton = new JButton(" Cancelar");
+		CancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		CancelButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		CancelButton.setBounds(384, 503, 150, 40);
 		ImageIcon cancelIcon = new ImageIcon("src//main//java//views//cancel.png");

c914d0603fdab1b269abc372e751efddfb918898
Javier <jfernandezfernandez.sanjose@alumnado.fundacionloyola.net>
2019-12-16 20:02:16 +0100

    remove other comments

--- royalHospital/src/main/java/views/InboxView.java
+++ royalHospital/src/main/java/views/InboxView.java
@@ -36,7 +36,7 @@ public class InboxView extends JFrame {
 
 	
 	/**
-	 * Launch the application.
+	 * Launch the application
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {

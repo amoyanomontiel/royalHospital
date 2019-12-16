453b3241455720bb73bbeef1bebe0facd134c352
Fernando Cañadas <fcanadasortega.sanjose@alumnado.fundacionloyola.net>
2019-12-16 21:27:58 +0100

    commit ventana inbox

--- royalHospital/src/main/java/views/InboxView.java
+++ royalHospital/src/main/java/views/InboxView.java
@@ -21,6 +21,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 
 public class InboxView extends JFrame {
 
@@ -28,12 +29,9 @@ public class InboxView extends JFrame {
 	private JPanel contentPane;
 	private final int width = 30;
 	private final int height = 30;
-	private JButton newMessageButton;
-	private JButton closeInboxButton;
-	private JButton returnInboxButton;
 	private JPanel inboxMailsPanel;
 	private JPanel messagePanel;
-	private static JPanel contextMail;
+	private JPanel headPanel;
 
 	
 	/**
@@ -57,18 +55,6 @@ public class InboxView extends JFrame {
 			}
 		});
 	}
-	
-	public void showInbox(JButton returnInboxButton, JButton newMessageButton, JButton closeInboxButton, JPanel inboxMailsPanel, JPanel messagePanel) {
-		returnInboxButton.setVisible(false);
-		newMessageButton.setVisible(true);
-		closeInboxButton.setVisible(true);
-	}
-	
-	public void showMessage(JButton returnInboxButton, JButton newMessageButton, JButton closeInboxButton, JPanel inboxMailsPanel, JPanel messagePanel) {
-		returnInboxButton.setVisible(true);
-		newMessageButton.setVisible(false);
-		closeInboxButton.setVisible(false);
-	}
 
 	/**
 	 * Create the frame.
@@ -80,72 +66,37 @@ public class InboxView extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		//contentPane.setLayout(null);
-		
-		JPanel headPanel = new JPanel();
-		headPanel.setBorder(null);
-		contentPane.add(headPanel);
-		//headPanel.setLayout(null);
-		
-		JLabel inboxLabel = new JLabel("Buzón De Entrada");
-		inboxLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		inboxLabel.setBounds(45, 16, 142, 27);
-		headPanel.add(inboxLabel);
-
-		
-		newMessageButton = new JButton("Redactar");
-		newMessageButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				showMessage(returnInboxButton, newMessageButton, closeInboxButton, inboxMailsPanel, messagePanel);
-			}
-		});
-		newMessageButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		newMessageButton.setBounds(462, 10, 150, 40);
 		ImageIcon newMailIcon = new ImageIcon("src/main/java/views/newGmail.png");
 		Icon newIcon = new ImageIcon(newMailIcon.getImage().getScaledInstance((width+10), height, Image.SCALE_DEFAULT));
-		newMessageButton.setIcon(newIcon);
-		headPanel.add(newMessageButton);
-		
-		closeInboxButton = new JButton("Volver");
-		closeInboxButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-		closeInboxButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		closeInboxButton.setBounds(660, 10, 150, 40);
 		ImageIcon newHomeIcon = new ImageIcon("src/main/java/views/homeIcon.png");
 		Icon newHome = new ImageIcon(newHomeIcon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
-		closeInboxButton.setIcon(newHome);
-		headPanel.add(closeInboxButton);
-		
-		returnInboxButton = new JButton("Volver");
-		returnInboxButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				showInbox(returnInboxButton, newMessageButton, closeInboxButton, inboxMailsPanel, messagePanel);
-			}
-		});
-		returnInboxButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		returnInboxButton.setBounds(660, 10, 150, 40);
 		ImageIcon newReturnIcon = new ImageIcon("src/main/java/views/return.png");
 		Icon newReturn = new ImageIcon(newReturnIcon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
-		returnInboxButton.setIcon(newReturn);
-		headPanel.add(returnInboxButton);
-		
-		JSeparator dividingLine = new JSeparator();
-		dividingLine.setForeground(new Color(255, 0, 51));
-		dividingLine.setBackground(new Color(255, 0, 51));
-		contentPane.add(dividingLine);
-		
-		showInbox(returnInboxButton, newMessageButton, closeInboxButton, inboxMailsPanel, messagePanel);
-		
-		contextMail = new JPanel();
-		contentPane.add(contextMail);
-		
-		inboxMailsPanel = objectMail.generateJComboBoxWithEmails(contextMail, contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		inboxMailsPanel.setBounds(0, 90, 836, 605);
 		contentPane.add(inboxMailsPanel);
+		
+		headPanel = new JPanel();
+		contentPane.add(headPanel, BorderLayout.NORTH);
+		
+		JLabel lblInbox = new JLabel("Buzón de entrada");
+		lblInbox.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
+		headPanel.add(lblInbox);
+		
+		JButton btnRefresh = new JButton("Refrescar");
+		btnRefresh.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		headPanel.add(btnRefresh);
+		
+		JButton btnWriteEmail = new JButton("Redactar");
+		btnWriteEmail.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		headPanel.add(btnWriteEmail);
+		
+		JButton btnCloseInbox = new JButton("Volver");
+		btnCloseInbox.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		headPanel.add(btnCloseInbox);
+		
+		JPanel contextMail = new JPanel();
+		contentPane.add(contextMail, BorderLayout.CENTER);
 
 
 	}

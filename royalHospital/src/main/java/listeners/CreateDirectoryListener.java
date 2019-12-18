fb8e9f1d8329edfcf170b37ead1e63c663c8dff7
DCS91 <dcuencasantamaria.sanjose@alumnado.fundacionloyola.net>
2019-12-18 13:03:06 +0100

    Creacion de listener del boton de crear directorio y ventana

---
+++ royalHospital/src/main/java/listeners/CreateDirectoryListener.java
@@ -0,0 +1,38 @@
+package listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import com.royalhospital.royalHospital.DataModel;
+
+import views.CreateModifyView;
+import views.MainRoyalView;
+
+public class CreateDirectoryListener implements ActionListener{
+	
+	private MainRoyalView royal;
+	private FTPClient client;
+	
+	public CreateDirectoryListener(MainRoyalView mainRoyalView, FTPClient ftpClient) {
+		this.client = ftpClient;
+		this.royal = mainRoyalView;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (DataModel.actualUserPath != "") {
+			if (FTPReply.isPositiveCompletion(client.getReplyCode())) {
+				CreateModifyView view = new CreateModifyView("Crear Directorio", "Introduce el nombre de directorio: ", "Crear Directorio", 0,client);
+				view.setVisible(true);
+				view.setLocationRelativeTo(null);
+			}
+		} else {
+			royal.getTxtaHistorial().append("Seleccione primero un directorio de la lista\n");
+		}
+		
+	}
+
+}

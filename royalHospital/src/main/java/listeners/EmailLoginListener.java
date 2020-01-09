d804bc4f076a7feaeb1ddbf0ec7a3c5a162f4f88
Fernando Cañadas <fcanadasortega.sanjose@alumnado.fundacionloyola.net>
2020-01-09 09:48:14 +0100

    add warning frame v2

--- royalHospital/src/main/java/listeners/EmailLoginListener.java
+++ royalHospital/src/main/java/listeners/EmailLoginListener.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 import views.InboxView;
 import views.MainMailView;
@@ -18,6 +19,10 @@ public class EmailLoginListener {
 			public void actionPerformed(ActionEvent e) {
 				if(MainMailView.getTxtUserName().getText().contains("@alumnado.fundacionloyola.net")) {
 					InboxView viewReceiveEmails = new InboxView();
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Formato de correo inválido", "Login Fallido",
+							JOptionPane.WARNING_MESSAGE);
 				}
 			}
 		});

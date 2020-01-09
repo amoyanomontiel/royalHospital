d2cb7047b4a8ddd6316a5572e986722aab41c732
Cristina <Cristina@TINAHS-LAPTOP>
2020-01-09 23:39:40 +0100

    comments

--- royalHospital/src/main/java/listeners/DownloadListener.java
+++ royalHospital/src/main/java/listeners/DownloadListener.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
 import com.royalhospital.royalHospital.AuxiliaryTools;
@@ -14,7 +13,9 @@ import com.royalhospital.royalHospital.DataModel;
 
 import views.ErrorRoyalView;
 import views.MainRoyalView;
+
 /**
+ * Download button listener class which download a file
  * 
  * @author Cristina Montilla
  *
@@ -24,11 +25,19 @@ public class DownloadListener implements ActionListener {
 	MainRoyalView mainRoyal;
 	FTPClient ftpClient;
 
+	/**
+	 * Initializes values of variables
+	 * 
+	 * @param MainRoyalView mainRoyal Principal frame
+	 * @param FTPClient ftpClient FTPClient
+	 */
 	public DownloadListener(MainRoyalView mainRoyal, FTPClient ftpClient) {
 		this.mainRoyal = mainRoyal;
 		this.ftpClient = ftpClient;
 	}
-
+/**
+ * Button event which checks if files exists and download it at user Download directory
+ */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		DataModel data = new DataModel();
@@ -40,14 +49,14 @@ public class DownloadListener implements ActionListener {
 						String sDirectoryWork = System.getProperty("user.home") + "/Downloads/";
 						File downloadDirectory = new File(sDirectoryWork + DataModel.selectedFile);
 						if (downloadDirectory.exists()) {
-							mainRoyal.getTxtaHistorial()
-									.append(data.getDownLoadExits()+ "\n");
+							mainRoyal.getTxtaHistorial().append(data.getDownLoadExits() + "\n");
 						} else {
 							FileOutputStream out = new FileOutputStream(sDirectoryWork + DataModel.selectedFile);
 							if (ftpClient.retrieveFile(DataModel.selectedFile, out)) {
-								AuxiliaryTools.saveOperationAtDBRecord(DataModel.codActualUser, "descargar", DataModel.selectedFile, 
-										AuxiliaryTools.actualDate(), AuxiliaryTools.actualTime());
-								mainRoyal.getTxtaHistorial().append(data.getDownLoadSuccess()+"\n");
+								AuxiliaryTools.saveOperationAtDBRecord(DataModel.codActualUser, "descargar",
+										DataModel.selectedFile, AuxiliaryTools.actualDate(),
+										AuxiliaryTools.actualTime());
+								mainRoyal.getTxtaHistorial().append(data.getDownLoadSuccess() + "\n");
 								mainRoyal.rootsToBlank();
 							} else {
 								mainRoyal.getTxtaHistorial().append(data.getDownLoadFail() + "\n");
